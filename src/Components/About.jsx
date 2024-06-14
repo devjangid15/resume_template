ece24d399ad33b775f5553e8c93efd00879398b7
devjangid15 <140658399+devjangid15@users.noreply.github.com>
2024-06-14 20:45:26 +0000

    commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,13 +10,13 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/img.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mathematics and Computing graduate from NSUT. I love to learn new Skills and technologies";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C++",
+  "Data Structure and Alogrithms (DSA)",
+  "JavaScript",
+  "FrontEnd Development",
+  "FullStack Development Learner",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am a BTech graduate having expertise in C++ & JavaScript.I have knowledge of Data Structures and Algorithm that help in my problem solving skills";
 
 const About = () => {
   return (
@@ -60,7 +59,14 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2
+          style={{
+            color: "black",
+            fontWeight: "bold",
+          }}
+        >
+          About Myself
+        </h2>
         <p className="large">{description}</p>
         <hr />
         <ul
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,15 +22,15 @@ import PropTypes from "prop-types";
  */
 import image from "../images/woman-with-tablet.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Dev_Jangid- C++ Developer";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div style={{ position: "absolute", top: "7rem", left: "10rem", width: "20rem" }}>
         <h1>{name}</h1>
-        <h2>{title}</h2>
+        <h2 style={{ marginTop: "20px", width: "34rem", fontSize: "50px" }}>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
